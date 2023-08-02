61a67afbc04fefe2709a801c3daecd1df267e59e
YerramSrinath <132989543+YerramSrinath@users.noreply.github.com>
2023-08-02 09:41:54 +0530

    Add files via upload

---
+++ vpcec2.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+    region = "us-west-2"  # Change this to your desired AWS region
+  access_key = "AKIA4MUN44V3QZ2ADQXD"
+  secret_key = "lo8n/j96x3k97KDmaB0DLnK0uSMrEUkKev5pXRur"
+  }
+  
+  # Create a new VPC
+  resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"  # Change this CIDR block to suit your requirements
+    tags = {
+      Name = "MyVPC"
+    }
+  }
+  
+  # Create a public subnet inside the VPC
+  resource "aws_subnet" "public_subnet" {
+    vpc_id                  = aws_vpc.my_vpc.id
+    cidr_block              = "10.0.1.0/24"  # Change this CIDR block to suit your requirements
+    availability_zone       = "us-west-2a"  # Change this to an available AZ in your region
+    map_public_ip_on_launch = true
+    tags = {
+      Name = "PublicSubnet"
+    }
+  }
+  
+  # Create an internet gateway (IGW) and attach it to the VPC
+  resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc.id
+  
+    tags = {
+      Name = "MyIGW"
+    }
+  }
+  
+  # Create a default route table and add a route to the IGW for the public subnet
+  resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.my_vpc.id
+  
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.my_igw.id
+    }
+  
+    tags = {
+      Name = "PublicRouteTable"
+    }
+  }
+  
+  # Associate the default route table with the public subnet
+  resource "aws_route_table_association" "public_subnet_association" {
+    subnet_id      = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_route_table.id
+  }
+  
